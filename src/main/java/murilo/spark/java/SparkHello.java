212fb6da5a1c9093f671372a5aa5f8084c05b8f7
Murilo Henrique Gomes <t1112crl@prestadorcbmp.com.br>
2020-08-20 21:28:12 -0300

    Lendo arquivo dataset

---
+++ src/main/java/murilo/spark/java/SparkHello.java
@@ -0,0 +1,36 @@
+package murilo.spark.java;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.*;
+
+import javax.xml.crypto.Data;
+import java.util.Arrays;
+import java.util.List;
+
+public class SparkHello {
+    public static void main(String[] args) {
+
+        SparkSession spark =  SparkSession.builder()
+                .master("local")
+                .appName("Word Count")
+                .getOrCreate();
+        Dataset<String> logData = spark.read().textFile( "/home/murilohg/Documentos/sparkFile/" );
+        long numAs = logData.filter((FilterFunction<String>) s -> s.contains("a")).count();
+        logData.show();
+
+
+
+
+
+//
+//        Dataset<Row> teenagersDF = spark.sql("SELECT name FROM people WHERE age BETWEEN 13 AND 19");
+//        Encoder<String> stringEncoder = Encoders.STRING();
+//        Dataset<String> teenagerNamesByIndexDF = teenagersDF.map(
+//                (MapFunction<Row, String>) row -> "Name: " + row.getString(0),
+//                stringEncoder);
+//        teenagerNamesByIndexDF.show();
+
+    }
+}
